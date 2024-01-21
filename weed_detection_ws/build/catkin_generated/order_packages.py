a852ec3e879dd4594dba3f5a98b5a7ba3c5463fd
rahgirrafi <rahgirrafi@gmail.com>
2024-01-21 13:33:00 +0600

    3 model saved

---
+++ weed_detection_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/media/rafi/Technical/Projects/weed_detection/weed_detection_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/media/rafi/Technical/ros/obj_detection_ws/devel;/media/rafi/Technical/ros/tutorial_ws/devel;/home/rafi/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/media/rafi/Technical/ros/obj_detection_ws/devel;/media/rafi/Technical/ros/tutorial_ws/devel;/home/rafi/catkin_ws/devel;/opt/ros/noetic' != '' else []
